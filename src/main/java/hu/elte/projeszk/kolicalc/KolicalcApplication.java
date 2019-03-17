1c865bc1e053d8ae850a844acef2b482f1de0347
Tamás Fejér <ftomi89@gmail.com>
2019-03-17 09:28:02 +0100

    Project init

---
+++ src/main/java/hu/elte/projeszk/kolicalc/KolicalcApplication.java
@@ -0,0 +1,13 @@
+package hu.elte.projeszk.kolicalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KolicalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KolicalcApplication.class, args);
+	}
+
+}
