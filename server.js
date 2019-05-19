e50990d99cfec1a600167e4b3c65a8c681f8e12e
Tamás Fejér <ftomi89@gmail.com>
2019-05-19 22:03:46 +0200

    server.js fix

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req, res) {
+/* app.get('/*', function(req, res) {
  res.sendFile(__dirname + '/dist/index.html');
-});
+}); */
 
 app.listen(process.env.PORT || 4200);
 console.log('SERVER RUNNING...');
