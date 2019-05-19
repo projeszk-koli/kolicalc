5c0e48028aaba9bb1950dfd31f0a4d5fb9a2f87a
Tamás Fejér <ftomi89@gmail.com>
2019-05-19 22:33:50 +0200

    Heroku fix v3

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/kolicalc-frontend'));
 
-app.get('/*', function(req, res) {
+/* app.get('/*', function(req, res) {
  res.sendFile(__dirname + '/dist/kolicalc-frontend/index.html');
-});
+}); */
 
 app.listen(process.env.PORT || 4200);
 console.log('SERVER RUNNING...');
